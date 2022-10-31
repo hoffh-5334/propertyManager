88e384a154b7d93b9e8e5ae2f0e0baa03c2fed38
SDann <smdann17@gmail.com>
2022-10-31 08:43:30 -0500

    Changed the way express looks for images in the server.js file. Added a helper for restricting length of text that displays for unit description in homepage.handlebars. Added image carousel to singleUnit.handlebars.

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,5 +2,13 @@
 module.exports = {
   format_date: (date) => {
     return date.toLocaleDateString();
+  },
+
+// Text length restriction
+  text_length_restrict: (str) => {
+    if (str.length > 175)
+    return str.substring(0,175) + '...';
+  return str;
   }
 };
+
